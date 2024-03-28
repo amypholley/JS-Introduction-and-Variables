2d5bdca460c09ffca1ae57246cb628a1cc0a6802
amyph <amypholley@gmail.com>
2024-03-28 12:05:28 -0500

    1

---
+++ apps.js
@@ -0,0 +1,28 @@
+// Exercise 1
+let firstName;
+let lastName;
+let age;
+
+// Exercise 2
+firstName = "Jamie";
+lastName = "Holley";
+age - 30;
+
+// Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = "19.99";
+let isOnSale = false;
+let salePercentage = 15;
+let inStock = false;
+let selectedSize = "M";
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
